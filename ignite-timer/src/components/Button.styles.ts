c631cf93234f8144447c4727412e77554db587ef
Gabriella Faria <93018901+gabriellafaria@users.noreply.github.com>
2023-08-07 19:37:51 +0000

    chore: add the Button component

---
+++ ignite-timer/src/components/Button.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'sucess'; 
+
+interface ButtonContainerProps {
+    variant: ButtonVariant; 
+}
+
+const buttonVariants ={
+    primary: 'purple',
+    secundary: 'orange',
+    danger: 'red',
+    sucess: 'green'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+
+    background-color: ${props => props.theme['green-500']};
+`
