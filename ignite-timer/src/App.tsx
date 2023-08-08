3fda4e75950c65a27bb7d3064cabc5a8b09b6fdf
gabriellafaria <gabisouf@gmail.com>
2023-08-07 21:17:52 -0300

    chore: use the react-router-dom

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,16 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
- 
