a0da4790c5ef5c0c19954faff4d1e6c19b275ea2
gabriellafaria <gabisouf@gmail.com>
2023-08-08 19:27:47 -0300

    fix: adjusting to follow the eslint

--- ignite-timer/src/pages/History/index.tsx
+++ ignite-timer/src/pages/History/index.tsx
@@ -1,8 +1,8 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
-    <HistoryContainer> 
+    <HistoryContainer>
       <h1>History</h1>
       <HistoryList>
         <table>
@@ -19,7 +19,9 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
           </tbody>
         </table>
