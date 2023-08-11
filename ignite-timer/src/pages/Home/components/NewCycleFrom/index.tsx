cfed66dfa7eeaf329d670211f9fa8ce39bc794ff
gabriellafaria <gabisouf@gmail.com>
2023-08-10 21:25:52 -0600

    fix: fixing the bugs in NewCycleForm - context

--- ignite-timer/src/pages/Home/components/NewCycleFrom/index.tsx
+++ ignite-timer/src/pages/Home/components/NewCycleFrom/index.tsx
@@ -1,25 +1,11 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import * as zod from 'zod'
-import { FormContainer, TaskInput } from './styles'
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
-    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
-})
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+import { FormContainer, MinutesAmoutInput, TaskInput } from './styles'
+import { useContext } from 'react'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+import { useFormContext } from 'react-hook-form'
 
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  })
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
 
   return (
     <FormContainer>
