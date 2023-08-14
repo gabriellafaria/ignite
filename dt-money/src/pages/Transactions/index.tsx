f50e4494529c49d918bd069c9b3983aef9b81710
Gabriella Faria <93018901+gabriellafaria@users.noreply.github.com>
2023-08-14 20:14:16 +0000

    chore: add the table and summary

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,33 @@
-import { Header } from "../../components/Header";
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/summary';
+import { SearchForm } from './components/SearchForm';
+import { TransationsConatiner, PriceHighLight, TransationsTable } from './styles';
 
 export function Transactions() {
     return (
         <div>
            <Header />
+           <Summary />
+
+           <TransationsConatiner> 
+            <SearchForm />
+            <TransationsTable>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de site</td>
+                        <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Hamburger</td>
+                        <PriceHighLight variant='outcome'>- R$ 59,00</PriceHighLight>
+                        <td>Alimentação</td>
+                        <td>10/04/2022</td>
+                    </tr>
+                </tbody>
+            </TransationsTable>
+           </TransationsConatiner>
         </div>
     )
 } 
