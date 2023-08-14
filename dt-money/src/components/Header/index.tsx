1910405e26b330014b63eb4195204c908334d53b
Gabriella Faria <93018901+gabriellafaria@users.noreply.github.com>
2023-08-14 13:27:10 +0000

    chore: init the Header component

---
+++ dt-money/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransationButton } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt="" />
+                <NewTransationButton>Nova transação</NewTransationButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+} 
