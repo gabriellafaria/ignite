14b32058f3f443f60679a8706277c741484aa7e1
gabriellafaria <gabisouf@gmail.com>
2023-08-15 14:52:25 -0600

    chore: implements the use of the context

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,37 +1,47 @@
-import { Header } from '../../components/Header';
-import { Summary } from '../../components/summary';
-import { SearchForm } from './components/SearchForm';
-import { TransationsConatiner, PriceHighLight, TransationsTable } from './styles';
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/summary'
+import { SearchForm } from './components/SearchForm'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import {
+  TransationsConatiner,
+  PriceHighLight,
+  TransationsTable,
+} from './styles'
+import { dateFormetter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
       <Summary />
 
-      <TransationsConatiner> 
-       <SearchForm />
+      <TransationsConatiner>
+        <SearchForm />
         <TransationsTable>
           <tbody>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Hamburger</td>
-              <td>
-                <PriceHighLight variant='outcome'>- R$ 59,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormetter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
-       </TransationsTable>
+        </TransationsTable>
       </TransationsConatiner>
     </div>
   )
-} 
+}
