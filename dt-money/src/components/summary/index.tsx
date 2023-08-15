fd5b061978e83495084ab814cbf0ced7846f2314
gabriellafaria <gabisouf@gmail.com>
2023-08-15 14:59:22 -0600

    chore: fix with the eslint rocketeseat config

--- dt-money/src/components/summary/index.tsx
+++ dt-money/src/components/summary/index.tsx
@@ -1,17 +1,17 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
-import { SummaryContainer, SummaryCard } from './styles';
-import { priceFormatter } from '../../utils/formatter';
-import { useSummary } from '../../hooks/useSummary';
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { SummaryContainer, SummaryCard } from './styles'
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
-export function Summary() { 
-  const summary = useSummary();
+export function Summary() {
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
           <span>Entrada</span>
-          <ArrowCircleUp size={32} color='#00B37E' />
+          <ArrowCircleUp size={32} color="#00B37E" />
         </header>
 
         <strong>{priceFormatter.format(summary.income)}</strong>
@@ -19,15 +19,15 @@ export function Summary() {
       <SummaryCard>
         <header>
           <span>Saídas</span>
-          <ArrowCircleDown size={32} color='#F75A68' />
+          <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
         <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
-      <SummaryCard variant='green'>
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
-          <CurrencyDollar size={32} color='#FFF' />
+          <CurrencyDollar size={32} color="#FFF" />
         </header>
 
         <strong>{priceFormatter.format(summary.total)}</strong>
