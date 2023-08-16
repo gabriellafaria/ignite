31e880710b413e5f401f102e7b3ed60da6bcefa0
gabriellafaria <gabisouf@gmail.com>
2023-08-15 19:11:19 -0600

    chore: modify the useContext for the useContextSelector

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/summary'
 import { SearchForm } from './components/SearchForm'
@@ -11,7 +11,9 @@ import {
 import { dateFormetter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>
