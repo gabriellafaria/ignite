323eb81cd9534f80af5c09a28b67bb937df52746
Gabriella Faria <93018901+gabriellafaria@users.noreply.github.com>
2023-08-17 19:55:26 +0000

    chore: init the Header

--- ignite-shop/src/pages/_app.tsx
+++ ignite-shop/src/pages/_app.tsx
@@ -1,3 +1,19 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { globalStyles } from '@/styles/global';
+import { AppProps } from 'next/app';
+import Image from 'next/image';
+
+import { Container, Header } from '@/styles/pages/app';
+const logo: string = require("../assets/logo.svg").default
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
